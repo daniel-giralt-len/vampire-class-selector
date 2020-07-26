a51d35fb113631c3598de75f37ad1d9c91babfc6
daniel-giralt-len <daniel.giralt.len@gmail.com>
2020-07-26 14:50:37 +0200

    implement clan list

---
+++ clan-data.js
@@ -0,0 +1,64 @@
+const clanData = {
+    "brujah": {
+        "name": "Brujah",
+        "text": "El somni dels Brujah és un món on tota injusticia és eliminada i vius i no-morts coexisteixen en pau. Mantenen que per amor als mortals és perquè es revolten. Però també pot ser que estiguin enfurismats contra la seva maledicció eterna. Inevitablement s'infiltren a les revolucions o les instiguen; inevitablement, l'Ànsia farà correr la sang; inevitablement, mai obtindran la pau.",
+        "logo": "assets/brujah-logo.png",
+        "image": "assets/brujah.png",
+        "sound": "assets/brujah.mp3"
+    },
+    "gangrel": {
+        "name": "Gangrel",
+        "text": "Entretant que d'altres Afillats maleeixen la seva fam i escullen la gàbia de la ciutat, els Gangrels accepten a la Bèstia i corren, lliures. Creuen fronteres entre espècies i nacions, amb la facilitat d'un depredador perfecte. Només els més forts sobreviuen a les seves caces. La natura els pertany, i ells pertanyen a la natura.",
+        "logo": "assets/gangrel-logo.png",
+        "image": "assets/gangrel.png",
+        "sound": "assets/Gangrel.mp3"
+    },
+    "malkàvien": {
+        "name": "Malkàvien",
+        "text": "Els psiquiatres diagnosticarien esquizofrència, depressió, trastorn obsessiu compulsiu o estrés postraumàtic als Malkàvien. A voltes, tot alhora. El cas és que tenen totes les afliccions i cap ni una. Els \"folls savis\" embogeixen de veure massa de l'univers al mateix temps, d'entendre massa la realitat i de sentir emocions massa fortes per fer-les-hi front. S'automediquen amb sang, però la bogeria sempre torna.",
+        "logo": "assets/malkavian-logo.png",
+        "image": "assets/malkavian.png",
+        "sound": "assets/Malkàvien.mp3"
+    },
+    "nosferatu": {
+        "name": "Nosferatu",
+        "text": "Pels Nosferatu, l'Abraçada suposa acceptar l'encarnació del mal... Literalment. La Sang els deforma el cos gradualment fins convertir-los en abominacions dantesques. Tal vegada el dolor els ensenyi humiltat i compassió: els Nosferatu són els més humans dels Afillats; duuen la maledicció per dins i no pas per fora. Per a passar desapercebuts, empren la Sang per canviar la seva cara a la de les seves víctimes, o simplement es fan fonedís.",
+        "logo": "assets/nosferatu-logo.png",
+        "image": "assets/Nosferatu.png",
+        "sound": "assets/Nosferatu.mp3"
+    },
+    "torejador": {
+        "name": "Torejador",
+        "text": "Menysprear a un Torejador per considerar-los pervertits promiscus o bé artistes eccèntrics és l'últim error que pots fer com a Afillat. La bellesa és poder, car l'amor per la bellesa pot fer que qualsevol persona faci qualsevol cosa. Vet aquí la promesa dels Torejador. Poden fer que els no-morts sentin quelcom real: una emoció humana.",
+        "logo": "assets/toreador-logo.png",
+        "image": "assets/toreador.png",
+        "sound": "assets/toreador.mp3"
+    },
+    "tremere": {
+        "name": "Tremere",
+        "text": "Tremer, un poderós mag del segle VIII, pensaba haver trobar l'elixir de la vida eterna. Gran fou llur sorpresa, veient-se encadenat a la Mlaedicció! Morí atemorit, però es despertà un cop més, malvingut a una eternitat de no-vida i Ànsia. Ara, els seus rituals màgics serveixen com a etern escarni, però que permeten bescanviar Sang fresca per manipular el propi teixit de la realitat.",
+        "logo": "assets/tremere-logo.png",
+        "image": "assets/tremere.png",
+        "sound": "assets/Tremere.mp3"
+    },
+    "ventrue": {
+        "name": "Ventrue",
+        "text": "Els reis per la desgràcia de Déu. Fan regles, castiguen als criminals i, de vegades, recompensen a qui els jura lleialtat. Els seus crítics els consideren dictadors o garjolers d'altres Afillats. Però la veritat és única: sense ells, la Mascarada, la Camarilla i, així, els vampirs haurien caigut fa molt de temps.",
+        "logo": "assets/ventrue-logo.png",
+        "image": "assets/ventrue.png",
+        "sound": "assets/Ventrue.mp3"
+    },
+    "caitiff": {
+        "name": "Caitiff",
+        "text": "No tots els Afillats pertànyen a un dels tretze clans. Alguns viuen entre les escletxes d'aquestes definicions; per accident o per voluntat, els Caitiff poden aprendre de hom però pertànyer enlloc. Compensen el seu poc pedigrí amb una Sang més flexible i una Ànsia i Maledicció més suportables.",
+        "image": "assets/caitiff.png",
+        "sound": "assets/Caitiff.mp3"
+    },
+    "sangFeble": {
+        "name": "Sang Feble",
+        "text": "Els Sang Feble, supervivents de dècades de mofes, prejudicis i ostracismes, han vingut per a quedar-se. La seva alquímia de sang, anàrquica i de carrer ja els fan versàtils. Sumant-hi a això la seva capacitat de fer-se passar per humans resultat en un vampir benaventurat per passar desapercebut dins la societat viva.",
+        "image": "assets/sang.png",
+        "sound": "assets/sang.mp3"
+    }
+}
+export default clanData
